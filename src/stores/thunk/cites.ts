6341ccbe696f414ff6dc887a6959b553e6e06291
The Apple <apple@macbook.local>
2021-09-27 20:58:40 +0700

    fix build failed

--- src/stores/thunk/cites.ts
+++ src/stores/thunk/cites.ts
@@ -8,7 +8,7 @@ export const handleGetCities = (params: GetCityParams) => async (dispatch: Dispa
     const { data } = await getCities(params);
     dispatch(citiesActions.getCitiesSuccess(data));
   } catch (error) {
-    dispatch(citiesActions.getCitiesFailed(error.message));
+    dispatch(citiesActions.getCitiesFailed('Get city failed'));
   }
 };
 
@@ -18,16 +18,16 @@ export const handleGetOldCities = (params: GetOldCityParams) => async (dispatch:
     const { data } = await getOldCities(params);
     dispatch(citiesActions.getOldCitiesSuccess(data));
   } catch (error) {
-    dispatch(citiesActions.getOldCitiesFailed(error.message));
+    dispatch(citiesActions.getOldCitiesFailed('Get old city failed'));
   }
 };
 
 export const handleGetPopulationEstimate = (params: GetOldCityParams) => async (dispatch: Dispatch) => {
   try {
     dispatch(citiesActions.getPopulationEstimateRequest());
-    const { data } = await getPopulationEstimate(params);
+    const { data }: any = await getPopulationEstimate(params);
     dispatch(citiesActions.getPopulationEstimateSuccess(data));
   } catch (error) {
-    dispatch(citiesActions.getPopulationEstimateFailed(error.message));
+    dispatch(citiesActions.getPopulationEstimateFailed('Get population estimate failed'));
   }
 };
